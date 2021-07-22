d758d88b90990c26d67647ae3fb275cc3d0dd946
Pablo Romero <pabloromerook@gmail.com>
2021-07-22 12:29:14 -0300

    First commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
+
+export const GifGrid = ({ category }) => {
+  //const [images, setImages] = useState([]);
+
+  const { data: images, loading } = useFetchGifs(category);
+
+  //getGifs();
+  return (
+    <>
+      <h3 className="animate__animated animate__fadeIn">{category}</h3>
+
+      {loading && (
+        <p className="animate__animated animate__flash">Loading...</p>
+      )}
+
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+//Consejos
+//En vez de poner img y luego img.id o img.title, lo desestructuramos
